b2c01af9466f24afd96da0c896d2c801770d863a
tomb <tombusstnz@gmail.com>
2026-05-20 22:57:09 +1200

    feat: only show toggle on instagram.com

--- popup/settings.js
+++ popup/settings.js
@@ -1,10 +1,23 @@
 const toggle = document.querySelector("#toggle");
+const popupContent = document.querySelector("#popup-content");
+const errorContent = document.querySelector("#error-content");
 
 (async () => {
+    const [tab] = await browser.tabs.query({ active: true, currentWindow: true });
+
+    // shows error text if not on instagram
+    if (!tab?.url?.includes("instagram.com")) {
+        popupContent.classList.add("hidden");
+        errorContent.classList.remove("hidden");
+        return;
+    }
+
+    // sets toggle state from local storage
     const { enabled } = await browser.storage.local.get("enabled");
     toggle.checked = enabled ?? true;
 })();
 
+// updates local storage when toggle changes
 toggle.addEventListener("change", () => {
     browser.storage.local.set({ enabled: toggle.checked });
 });
